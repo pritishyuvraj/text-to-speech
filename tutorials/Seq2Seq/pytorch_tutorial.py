ebacb00bf9465ea1c1d9c5d58fd8464ec9bdcafa
Pritish Yuvraj <pyuvraj@fb.com>
2019-09-03 12:40:08 -0700

    training

--- tutorials/Seq2Seq/pytorch_tutorial.py
+++ tutorials/Seq2Seq/pytorch_tutorial.py
@@ -1,16 +1,21 @@
-from __future__ import unicode_literals, print_function, division
-from io import open
-import unicodedata
-import string
-import re
+from __future__ import division, print_function, unicode_literals
+
 import random
+import re
+import string
+import time
+import unicodedata
+from io import open
+from pprint import pprint
 
 import torch
 import torch.nn as nn
-from torch import optim
 import torch.nn.functional as F
+from helper_func import asMinutes, showPlot, timeSince
+from model import AttnDecoderRNN, DecoderRNN, EncoderRNN
+from torch import optim
 
-from pprint import pprint
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 SOS_token = 0
@@ -47,7 +52,9 @@ class Lang:
 
 
 def unicodeToAscii(s):
-    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+    return "".join(
+        c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
+    )
 
 
 def normalizeString(s):
@@ -59,9 +66,8 @@ def normalizeString(s):
 
 def readLangs(lang1, lang2, reverse=False):
     print("Reading lines ...")
-    read_file = open("./data/fra.txt",
-                     encoding='utf-8').read().strip().split('\n')
-    pairs = [[normalizeString(s) for s in l.split('\t')] for l in read_file]
+    read_file = open("./data/fra.txt", encoding="utf-8").read().strip().split("\n")
+    pairs = [[normalizeString(s) for s in l.split("\t")] for l in read_file]
     if reverse:
         pairs = [list(reversed(p)) for p in pairs]
         input_lang = Lang(lang2)
@@ -74,19 +80,27 @@ def readLangs(lang1, lang2, reverse=False):
 
 MAX_LENGTH = 10
 eng_prefixes = (
-    "i am ", "i m ",
-    "he is", "he s ",
-    "she is", "she s ",
-    "you are", "you re ",
-    "we are", "we re ",
-    "they are", "they re "
+    "i am ",
+    "i m ",
+    "he is",
+    "he s ",
+    "she is",
+    "she s ",
+    "you are",
+    "you re ",
+    "we are",
+    "we re ",
+    "they are",
+    "they re ",
 )
 
 
 def filterPair(p):
-    return len(p[0].split(' ')) < MAX_LENGTH and \
-        len(p[1].split(' ')) < MAX_LENGTH and \
-        p[1].startswith(eng_prefixes)
+    return (
+        len(p[0].split(" ")) < MAX_LENGTH
+        and len(p[1].split(" ")) < MAX_LENGTH
+        and p[1].startswith(eng_prefixes)
+    )
 
 
 def filterPairs(pairs):
@@ -109,7 +123,7 @@ def prepareData(lang1, lang2, reverse=False):
 
 
 def indexesFromSentence(lang, sentence):
-    return [lang.word2index[word] for word in sentence.split(' ')]
+    return [lang.word2Index[word] for word in sentence.split(" ")]
 
 
 def tensorFromSentence(lang, sentence):
@@ -124,5 +138,125 @@ def tensorsFromPair(pair):
     return (input_tensor, target_tensor)
 
 
-input_lang, output_lang, pairs = prepareData('eng', 'fra', reverse=True)
+input_lang, output_lang, pairs = prepareData("eng", "fra", reverse=True)
 print(random.choice(pairs))
+
+teacher_forcing_ratio = 0.5
+
+
+def train(
+    input_tensor,
+    target_tensor,
+    encoder,
+    decoder,
+    encoder_optimizer,
+    decoder_optimizer,
+    criterion,
+    max_length=MAX_LENGTH,
+):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+def trainIters(
+    encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01
+):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0
+    plot_loss_total = 0
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [tensorsFromPair(random.choice(pairs)) for i in range(n_iters)]
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+
+        loss = train(
+            input_tensor,
+            target_tensor,
+            encoder,
+            decoder,
+            encoder_optimizer,
+            decoder_optimizer,
+            criterion,
+        )
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print(
+                "%s (%d %d%%) %.4f"
+                % (
+                    timeSince(start, iter / n_iters),
+                    iter,
+                    iter / n_iters * 100,
+                    print_loss_avg,
+                )
+            )
+
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+    showPlot(plot_losses)
+
+
+hidden_size = 256
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(
+    device
+)
+
+trainIters(encoder1, attn_decoder1, 75000, print_every=500)
