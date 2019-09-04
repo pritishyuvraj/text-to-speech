ad571a9aa4f22bba3ba85a1bc566652e280d2ea2
Pritish Yuvraj <pyuvraj@fb.com>
2019-09-03 18:57:43 -0700

    training

--- tutorials/Seq2Seq/pytorch_tutorial.py
+++ tutorials/Seq2Seq/pytorch_tutorial.py
@@ -15,6 +15,9 @@ from helper_func import asMinutes, showPlot, timeSince
 from model import AttnDecoderRNN, DecoderRNN, EncoderRNN
 from torch import optim
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -259,4 +262,88 @@ attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).
     device
 )
 
-trainIters(encoder1, attn_decoder1, 75000, print_every=500)
+trainIters(encoder1, attn_decoder1, 1000, print_every=500)
+
+
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)
+
+        decoder_hidden = encoder_hidden
+
+        decoder_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoder_words.append("<EOS>")
+                break
+            else:
+                decoder_words.append(output_lang.index2Word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoder_words, decoder_attentions[: di + 1]
+
+
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print(">", pair[0])
+        print("=", pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = " ".join(output_words)
+        print("<", output_sentence)
+        print("")
+
+
+evaluateRandomly(encoder1, attn_decoder1)
+
+output_words, attentions = evaluate(encoder1, attn_decoder1, "je suis trop froid .")
+plt.matshow(attentions.numpy())
+
+
+def showAttention(input_sentence, output_words, attentions):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap="bone")
+    fig.colorbar(cax)
+
+    ax.set_xticklabels([""] + input_sentence.split("") + ["<EOS>"], rotation=90)
+    ax.set_yticklabels([""] + output_words)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+
+def evaluateAndShowAttention(input_sentence):
+    outputs_words, attentions = evaluate(encoder1, attn_decoder1, input_sentence)
+    print("input =", input_sentence)
+    print("output =", "".join(output_words))
+    showAttention(input_sentence, output_words, attentions)
+
+
+evaluateAndShowAttention("elle a cinq ans de moins que moi .")
+
+evaluateAndShowAttention("elle est trop petit .")
+
+evaluateAndShowAttention("je ne crains pas de mourir .")
+
+evaluateAndShowAttention("c est un jeune directeur plein de talent .")
